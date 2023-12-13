06d79492654d47d69554daa370e56f27cf5347d4
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-13 19:34:32 +0300

    :package: Added @nestjs/jwt and passport packages :rocket:

---
+++ server/src/auth/jwt/jwt.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class JwtService {
+  private readonly secretKey = process.env.JWT_SECRET_KEY;
+
+  generateToken(payload: any): string {
+    return jwt.sign(payload, this.secretKey, { expiresIn: '1h' });
+  }
+
+  verifyToken(token: string): any {
+    return jwt.verify(token, this.secretKey);
+  }
+
+  generateRefreshToken(payload: any): string {
+    return jwt.sign(payload, this.secretKey, { expiresIn: '7d' });
+  }
+
+  verifyRefreshToken(token: string): any {
+    return jwt.verify(token, this.secretKey);
+  }
+}
