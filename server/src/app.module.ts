8284ba90664c6261bb7c24f8332a25b80d24674f
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-13 23:21:17 +0300

    ✨ Added DbModule to AppModule imports 🚀

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ElasticsearchModule } from './elasticsearch/elasticsearch.module';
 import { AuthModule } from './auth/auth.module';
-import { DbModule } from './postgres/postgres.module';
-import { FactoryDetailModule } from './factories/factoryDetail/factoryDetail.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FactoryListModule } from './factories/factoryList/factoryList.module';
+import { DbModule } from './postgres/postgres.module';
+import { FactoryDetailModule } from './factories/factoryDetail/factoryDetail.module';
 
 @Module({
   imports: [ElasticsearchModule,AuthModule,DbModule,FactoryListModule,FactoryDetailModule],
