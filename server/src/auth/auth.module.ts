f16b0824163d44297d2af6cf826a0ac0f15f90b0
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-13 19:56:56 +0300

    :closed_lock_with_key: Added JwtService to providers in AuthModule :rocket:

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ElasticsearchModule } from '../elasticsearch/elasticsearch.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtService } from './jwt/jwt.service';
 
 @Module({
   imports: [ElasticsearchModule], 
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService,JwtService],
 })
 export class AuthModule {}
