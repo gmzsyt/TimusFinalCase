4fdcee439261088dc6e1fdc50f3b1324b7f22a58
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-13 19:07:00 +0300

    :key: Transitioned to JWT Guard structure for authentication :rocket:

---
+++ server/src/auth/jwt/jwt-auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { JwtService } from './jwt.service';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const decoded = this.jwtService.verifyToken(token);
+      request['user'] = decoded;
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
