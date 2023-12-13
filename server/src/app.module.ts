29d59575d25a8b66ee8d97a5b606d78bbcda01ba
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-14 00:14:57 +0300

    Resolved configuration issue :rocket:

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,17 @@
-import { FactoryListModule } from './factories/factoryList/factoryList.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ElasticsearchModule } from './elasticsearch/elasticsearch.module';
 import { AuthModule } from './auth/auth.module';
 import { DbModule } from './postgres/postgres.module';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { FactoryListModule } from './factories/factoryList/factoryList.module';
 import { FactoryDetailModule } from './factories/factoryDetail/factoryDetail.module';
 
 @Module({
   imports: [ElasticsearchModule,AuthModule,DbModule,FactoryListModule,FactoryDetailModule],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
