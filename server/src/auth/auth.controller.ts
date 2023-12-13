e27bec6ec891b194072febd6d0916f92f820faaf
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-13 16:10:18 +0300

    :inbox_tray: Added login, register, getAllUsers, getUserDetails, and deleteUser endpoints to AuthController :rocket:

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Post, Get, Body, Param, Delete, Put } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserLoginDTO } from './dtos/user-login.dto';
+import { UserRegisterDTO } from './dtos/user-register.dto';
+
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+async login(@Body() userLoginDTO: UserLoginDTO): Promise<any> {
+  return this.authService.login(userLoginDTO);
+}
+
+  @Post('register')
+  async register(@Body() userDTO: UserRegisterDTO): Promise<any> {
+    return this.authService.register(userDTO);
+  }
+
+  @Get('getAllUsers')
+  async getAllUsers(): Promise<any> {
+    return this.authService.getAllUsers();
+  }
+
+  @Get('getUserDetails/:username')
+  async getUserDetails(@Param('username') username: string): Promise<any> {
+    return this.authService.getUserDetails(username);
+  }
+  
+   // !! put   
+
+
+  @Delete('deleteUser/:username') 
+  async deleteUser(@Param('username') username: string): Promise<any> {
+  return this.authService.deleteUser(username);
+ }
+}
