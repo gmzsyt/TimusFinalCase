cf022d0c6cf49246346a0760aeb2751c095b0877
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-13 20:24:17 +0300

    :file_folder: Created 'constants.ts' and set up PostgreSQL connection in 'postgres.provider.ts' :rocket:

---
+++ server/src/postgres/postgres.provider.ts
@@ -0,0 +1,49 @@
+import { Pool } from 'pg';
+import { PG_CONNECTION } from '../../constants';
+
+export const dbProvider = {
+  provide: PG_CONNECTION,
+  useFactory: async () => {
+    const pool = new Pool({
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      port: parseInt(process.env.PG_PORT, 10)
+    });
+
+    const client = await pool.connect();
+
+    try {
+      await client.query(`
+        CREATE TABLE IF NOT EXISTS factory_list (
+          id SERIAL PRIMARY KEY,
+          company_name TEXT,
+          membership_start_date DATE,
+          membership_end_date DATE,
+          employee_count INTEGER,
+          free_member BOOLEAN
+        );
+      `);
+      console.log("tablo oluşturuldu 1")
+
+      await client.query(`
+        CREATE TABLE IF NOT EXISTS factory_detail (
+          id SERIAL PRIMARY KEY,
+          factory_id INTEGER REFERENCES factory_list(id),
+          using_unit TEXT,
+          date_range TSRANGE,
+          usage_kw INTEGER,
+          usage_fee NUMERIC,
+          discounted_price BOOLEAN
+        );
+      `);
+      console.log("tablo oluşturuldu 2")
+
+
+      return client;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
