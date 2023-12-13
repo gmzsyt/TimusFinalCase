fa608cdc9fc091fa9e6ea8d938963054b3435056
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-13 21:06:07 +0300

    :computer: Implemented 'FactoryListController' for 'factory list' operations :rocket:
    ● Added controller for factory list management

---
+++ server/src/factories/factory-list/factory-list.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { FactoryListService } from './factory-list.service';
+import { FactoryListCreateDTO } from './dtos/factoryListCreate.dto';
+import { FactoryListUpdateDTO } from './dtos/factoryListUpdate.dto';
+
+@Controller('factory-list')
+export class FactoryListController {
+  constructor(private readonly factoryListService: FactoryListService) {}
+
+  @Get()
+  async findAll(): Promise<any[]> {
+    return this.factoryListService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<any> {
+    return this.factoryListService.findById(+id);
+  }
+
+  @Post()
+  async create(@Body() factoryListCreateDTO: FactoryListCreateDTO): Promise<any> {
+    return this.factoryListService.create(factoryListCreateDTO);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() factoryListUpdateDTO: FactoryListUpdateDTO): Promise<any> {
+    return this.factoryListService.update(+id, factoryListUpdateDTO);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.factoryListService.delete(+id);
+  }
+}
