327e42c835ecf511178974656173d583fa0119f8
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-13 16:27:11 +0300

    :heavy_plus_sign: Added AuthModule to AppModule imports :rocket: ● Followed module structuring best practices

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ElasticsearchModule } from './elasticsearch/elasticsearch.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ElasticsearchModule],
+  imports: [ElasticsearchModule,AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
