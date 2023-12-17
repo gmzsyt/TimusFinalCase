50685312f9f38fde955ec87a80f1e5fe233870b3
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-17 20:22:28 +0300

    ➕ Added navbar to 'Settings' view :rocket:

--- client/src/views/SettingsView.vue
+++ client/src/views/SettingsView.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>
-      <h1>settings</h1>
-      <!-- Fabrica Listesi sayfasının içeriğini buraya ekleyebilirsiniz -->
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    // Gerekirse bileşenle ilgili mantıksal kodlar
-  };
-  </script>
-  
+  <div>
+    <Navbar />
+    <h1>Settings</h1>
+    <!-- Settings sayfasının içeriğini buraya ekleyebilirsiniz -->
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/NavbarComp.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
