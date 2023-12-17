241feb4b288596644129ac72893418f693d9bfc5
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-17 21:18:15 +0300

    📁 Organized file structures for better clarity

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,14 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/HomeView.vue';
-import Login from '@/views/LoginView.vue';
-import Register from '@/views/RegisterView.vue';
-import Settings from '@/views/SettingsView.vue';
-import FabricaList from '@/views/FabricaListView.vue';
+import Login from '@/views/account/LoginView.vue';
+import Register from '@/views/account/RegisterView.vue';
+import Settings from '@/views/users/SettingsView.vue';
+import FabricaList from '@/views/factories/FabricaListView.vue';
+import FactoryDetail from '@/views/factories/FactoryDetailView.vue';
 const routes = [
   { path: '/', component: Home, meta: { requiresAuth: true } },
   { path: '/login', component: Login },
   { path: '/register', component: Register },
-  { path: '/settings', component: Settings, meta: { requiresAuth: true } }, // Yeni eklenen rota
+  { path: '/settings', component: Settings, meta: { requiresAuth: true } },
+  { path: '/detail', component: FactoryDetail, meta: { requiresAuth: true } }, 
   { path: '/fabricaList', component: FabricaList, meta: { requiresAuth: true } }, // Yeni eklenen rota
 ];
 
