819513dcc5f5538066d9135982e18445e7bdee8a
Gamze Seyit <82291548+gmzsyt@users.noreply.github.com>
2023-12-18 20:08:37 +0300

    chore(navbar): update navbar

--- client/src/components/NavbarComp.vue
+++ client/src/components/NavbarComp.vue
@@ -1,31 +1,10 @@
 <template>
-<<<<<<< HEAD
-
-=======
-<<<<<<< Updated upstream
->>>>>>> frontend-feature-login-register-views
-  <v-container class="text-center">
-    <v-app>
-      <v-app-bar app dark color=#FAF1E4>
-        <!-- Sol tarafta bulunan logo -->
-=======
   <v-container>
     <v-app>
       <v-app-bar app dark color="#FAF1E4">
->>>>>>> Stashed changes
         <v-img src="@/assets/logos.png" class="mr-4 align-start"></v-img>
         <v-toolbar-title>Vuetify App</v-toolbar-title>
-<<<<<<< Updated upstream
-
-<<<<<<< HEAD
-
-        <!-- Sağ tarafta bulunan kullanıcı adı, çıkış yap butonu ve dashboard butonu -->
-=======
-        <!-- Sağ tarafta bulunan kullanıcı adı ve çıkış yap butonu -->
-=======
         
->>>>>>> Stashed changes
->>>>>>> frontend-feature-login-register-views
         <v-spacer></v-spacer>
 
         <template v-if="isLoggedIn">
@@ -34,21 +13,9 @@
         </template>
 
         <template v-else>
-<<<<<<< HEAD
-          <v-btn @click="goToLogin" :style="{ 'color': 'white', 'background-color': '#435334', 'margin-right': '8px' }">Login</v-btn>
-          <v-btn @click="goToRegister" :style="{ 'color': 'white', 'background-color': '#435334','margin-right': '8px' }">Register</v-btn>
-          <v-btn @click="goToSettings" :style="{ 'color': 'white', 'background-color': '#435334', 'margin-right': '8px' }">Settings</v-btn>
-          <v-btn @click="goToDashboard" :style="{ 'color': 'white', 'background-color': '#435334' }">Dashboard</v-btn>
-=======
-<<<<<<< Updated upstream
-          <v-btn text @click="goToLogin">Login</v-btn>
-          <v-btn text @click="goToRegister">Register</v-btn>
-=======
           <v-btn v-if="username" :style="{ 'color': 'white', 'background-color': '#435334' }">{{ username }}</v-btn>
           <v-btn @click="goToLogin" :style="{ 'color': 'white', 'background-color': '#435334', 'margin-right': '8px' }">Giriş Yap</v-btn>
           <v-btn @click="goToRegister" :style="{ 'color': 'white', 'background-color': '#435334', 'margin-right': '8px' }">Kayıt Ol</v-btn>
->>>>>>> Stashed changes
->>>>>>> frontend-feature-login-register-views
         </template>
 
         <v-btn v-if="isLoggedIn" @click="goToSettings" :style="{ 'color': 'white', 'background-color': '#435334', 'margin-right': '8px' }">Ayarlar</v-btn>
@@ -85,36 +52,15 @@ export default {
       this.$store.dispatch('useUserStore/setUserData', null);
       this.$router.push('/login');
     },
-<<<<<<< HEAD
-
-    goToSettings() {
-      // Settings sayfasına yönlendirme
-      this.$router.push('/settings');
-    },
-    goToDashboard() {
-      // Dashboard sayfasına yönlendirme
-      this.$router.push('/fabricaList');
-    },
-  },
-  mounted() {
-    // Burada kullanıcının giriş yapmış olup olmadığını kontrol edebilirsiniz.
-    // Eğer giriş yapmışsa, this.isLoggedIn'ı true olarak ve this.username'ı ilgili kullanıcı adıyla ayarlayın.
-    // Örneğin, cookie'den veya başka bir kimlik doğrulama yönteminden bilgileri çekebilirsiniz.
-=======
-<<<<<<< Updated upstream
-=======
     goToSettings() {
       this.$router.push('/settings');
     },
     goToDashboard() {
       this.$router.push('/dashboard');
     },
->>>>>>> Stashed changes
->>>>>>> frontend-feature-login-register-views
   },
 };
 </script>
 
 <style scoped>
-/* İhtiyaca göre özel stiller ekleyebilirsiniz. */
 </style>
